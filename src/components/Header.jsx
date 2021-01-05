8374d03fd62cefd5fa0c946a34886bbd482dc3d4
Agus Mailen <marrapodiagustina@gmail.com>
2021-01-05 00:43:43 -0300

    Header nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 // import { connect } from 'react-redux';
+import logo from '../assets/static/iconHeader.jpg';
 import '../assets/styles/components/Header.scss';
 
 const Header = (props) => {
     return (
         <header>
+            <a href='/'>
+                <img className='header__img' src={logo} alt='icon' />
+            </a>
             <div className='header__menu'>
-                <p>Iniciar Sesión</p>
+                <div>
+                    <ul>
+                        <li>Mi Trabajo</li>
+                        <li>Iniciar Sesión</li>
+                        <li>Contacto</li>
+                    </ul>
+                </div>
             </div>
         </header>
     );
