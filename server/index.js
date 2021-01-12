2b1edcaa7165335a19c46e9c73420b048f078c56
Agus Mailen <marrapodiagustina@gmail.com>
2021-01-12 15:50:45 -0300

    user model

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const { config } = require('./config/config');
 
+const mongoose = require('mongoose');
+const url = `mongodb://localhost:27017/Winil`;
+mongoose.connect(url, { useNewUrlParser: true }, error => error ? console.error(`MongoDB Conneciton error: ${error}`) : null);
+mongoose.connection.on('connected', () => console.info("[MongoDB] Connected to " + url));
+
 app.listen((config.port), () => {
-	console.log(`Server runing at ${config.host}:${config.port}`)
+	console.log(`Server running on ${config.host}:${config.port}`)
 });
---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const userSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	phone: {
+		type: Number,
+		required: true,
+		unique: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	active: {
+		type: Boolean,
+		required: true
+	}
+});
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
