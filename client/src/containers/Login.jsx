3f85bc559dbf4bf62f91c3086bbeba98212e0630
Agus Mailen <marrapodiagustina@gmail.com>
2021-01-14 14:47:33 -0300

    arreglo de manejo de errores

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import '../assets/styles/components/Login.scss';
